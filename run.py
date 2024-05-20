e6d00608e9c7d10c2606db8cd09b88ac3f3c9e7d
daviddflix <daviddflix@gmail.com>
2024-05-20 14:03:54 -0300

    fix run

--- run.py
+++ run.py
@@ -51,8 +51,9 @@ def create_db(app):
         initialize_fixed_data()
         initialize_sites_data()
         initialize_keywords()
-
+ 
 
 if __name__ == "__main__":
     create_db(app)
-    app.run(debug=True, use_reloader=False, host='0.0.0.0', port=5000)
+    app.run(debug=False, use_reloader=False, port=5000)
+    # app.run(debug=True, use_reloader=True, host='0.0.0.0', port=5000)
