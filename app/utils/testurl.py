001dead2643888c7f26ce0d57ff8a4424e41c7a5
agustin bustamante <agustinbustamante@Agustins-MBP-2.fibertel.com.ar>
2024-07-23 01:19:24 -0300

    pulish routes to redirect

--- app/utils/testurl.py
+++ app/utils/testurl.py
@@ -28,7 +28,4 @@ def resolve_redirects_playwright(url: str) -> str:
         print(f"Error using Playwright: {e}")
         return None
 
-# Example usage
-if __name__ == "__main__":
-    test_url = 'https://news.google.com/articles/CBMidmh0dHBzOi8vd3d3LmNvaW5kZXNrLmNvbS9idXNpbmVzcy8yMDI0LzA3LzIyL3N3YW4tYml0Y29pbi1kcm9wcy1pcG8tcGxhbi1jdXRzLXN0YWZmLWFuZC13aWxsLXNodXQtbWFuYWdlZC1taW5pbmctdW5pdC_SAXpodHRwczovL3d3dy5jb2luZGVzay5jb20vYnVzaW5lc3MvMjAyNC8wNy8yMi9zd2FuLWJpdGNvaW4tZHJvcHMtaXBvLXBsYW4tY3V0cy1zdGFmZi1hbmQtd2lsbC1zaHV0LW1hbmFnZWQtbWluaW5nLXVuaXQvYW1wLw?hl=en-US&gl=US&ceid=US%3Aen'
-    resolve_redirects_playwright(test_url)
+
