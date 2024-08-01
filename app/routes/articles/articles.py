a27da1bf4e2934cfc905f66925e7b219db98d037
aialphanovatide <support@aialpha.ai>
2024-08-01 23:37:38 +0100

    merge

--- app/routes/articles/articles.py
+++ app/routes/articles/articles.py
@@ -348,7 +348,7 @@ def extract_text_from_google_docs(link):
     """
     try:
         with sync_playwright() as p:
-            browser = p.chromium.launch(headless=False)
+            browser = p.chromium.launch(headless=True)
             context = browser.new_context()
             page = context.new_page()
             page.goto(link)
